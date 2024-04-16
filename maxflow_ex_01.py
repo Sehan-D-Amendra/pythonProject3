251837ebd9f6201ae53c270c28fdc21d927b1e30
Sehan-D-Amendra <sdamendra03@gmail.com>
2024-04-16 16:00:15 +0530

    Initial commit

---
+++ maxflow_ex_01.py
@@ -0,0 +1,45 @@
+import networkx as nx
+
+G = nx.DiGraph()
+
+G.add_edge("1","2", capacity = 10)
+G.add_edge("1","3", capacity = 1)
+G.add_edge("1","4", capacity = 7)
+G.add_edge("2","5", capacity = 5)
+G.add_edge("2","4", capacity = 1)
+G.add_edge("3","4", capacity = 6)
+G.add_edge("3","6", capacity = 5)
+G.add_edge("4","5", capacity = 2)
+G.add_edge("4","7", capacity = 3)
+G.add_edge("4","6", capacity = 4)
+G.add_edge("5","7", capacity = 9)
+G.add_edge("6","7", capacity = 8)
+
+flow_value, flow_dict = nx.maximum_flow(G, "1","7")
+
+print("Maximum Flow :", flow_value)
+
+for key_i, inner_dict in flow_dict.items():
+    for key_j, inner_val in inner_dict.items():
+        print(f' {key_i} -> {key_j} \t Flow : {inner_val}')
+
+
+#using the maximum flow minimum cut theorem
+
+cut_value, partition = nx. minimum_cut(G, "1","7")
+
+print("Capacity of minimum cut :" , cut_value)
+print("Partitions: ", partition)
+
+reachabel, non_reachabel = partition
+
+#forward cut
+forwardcut = set()
+for u, nbrs in((n, G[n]) for n in reachabel) : forwardcut.update((u,v) for v in nbrs if v in non_reachabel)
+print("Forward cut set: ", sorted(forwardcut))
+
+
+#revers cut
+reverscut = set()
+for u, nbrs in((n, G[n]) for n in non_reachabel) : reverscut.update((u,v) for v in nbrs if v in reachabel)
+print("Revers cut set: ", sorted(reverscut))
